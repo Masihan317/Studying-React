5dda5d8e0ed164b1b68a9d022cf2bc2c7c348b24
sihanm2 <sihanm2@uw.edu>
2023-06-28 17:33:31 -0700

    2:52:45

--- src/index.js
+++ src/index.js
@@ -2,40 +2,38 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL210_SR195,210_.jpg',
+    title: 'Happy Place',
+    author: 'Emily Henry'
+  }
+  ,
+  {
+    img: 'https://m.media-amazon.com/images/I/51JSo2gyr2L._SX331_BO1,204,203,200_.jpg',
+    title: 'Nick and Charlie',
+    author: 'Alice Oseman'
+  },
+];
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return (<Book book={book}></Book>);
+      })}
     </section>
     );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props.book;
   return (
   <article className='book'>
-    <Image />
-    <Title />
-    <Author />
+    <img src={img} alt='book'/>
+    <h1>{title}</h1>
+    <h4>{author}</h4>
   </article>);
 };
-
-const Image = () => {
-  return (<img src='https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL210_SR195,210_.jpg' alt='book'/>);
-}
-
-const Author = () => {
-  return <h4 style={{ color:'#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Emily Henry</h4>;
-}
-
-const Title = () => {
-  return <h1>Happy Place</h1>;
-}
 
 ReactDom.render(<BookList />, document.getElementById('root'));
